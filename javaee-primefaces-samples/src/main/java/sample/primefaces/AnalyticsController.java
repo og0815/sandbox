2e5868ca82fecd205edb12b88ee864ed53406f99
Oliver Günther <oliver.guenther@gg-net.de>
2015-04-15 16:09:14 +0200

    some faces experiments

--- javaee-primefaces-samples/src/main/java/sample/primefaces/AnalyticsController.java
+++ javaee-primefaces-samples/src/main/java/sample/primefaces/AnalyticsController.java
@@ -1,15 +1,18 @@
 package sample.primefaces;
 
-
 import java.io.Serializable;
 import java.util.*;
-import javax.faces.bean.ManagedBean;
+import javax.annotation.ManagedBean;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
 
 /**
  *
  * @author oliver.guenther
  */
+@Named
 @ManagedBean
+@ViewScoped
 public class AnalyticsController implements Serializable {
 
     private String customer;
@@ -17,6 +20,7 @@ public class AnalyticsController implements Serializable {
     private final static List<String> names = Arrays.asList("Max Mustermann", "Max Malström", "Moritz Mossman", "Mika Miesmuschel", "Hugo Holstein");
 
     public List<String> completeCustomer(String query) {
+        System.out.println("Hohoh"); // TODo: Bug somethere somehow.
         List<String> result = new ArrayList<>();
         for (String name : names) {
             if (name.startsWith(query)) result.add(name);
@@ -32,6 +36,11 @@ public class AnalyticsController implements Serializable {
 
     public void setCustomer(String customer) {
         this.customer = customer;
+    }
+
+    public String getMessage() {
+        System.out.println("Horst im Wald");
+        return "Huhu 4 : " + new Date();
     }
 
 }
